26a041d777f714e2d6032340c6298fbc40863296
Benito Perdomo <benito.perdomo.v@gmail.com>
2022-03-16 15:21:36 -0600

    download repos

---
+++ 02GetRepos.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/PDNMX/piloto_sistema_catalogos.git
+git clone https://github.com/PDNMX/piloto_sistema_oauth20.git
+git clone https://github.com/PDNMX/piloto_sistema_backend.git
+git clone https://github.com/PDNMX/piloto_sistema_frontend.git
