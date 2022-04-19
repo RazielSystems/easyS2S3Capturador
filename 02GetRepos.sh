850b86cbc255e25cd67af4a6be2d38598f0d0217
Benito Perdomo <benito.perdomo.v@gmail.com>
2022-04-19 15:32:38 -0500

    UPDATE repos ssh

--- 02GetRepos.sh
+++ 02GetRepos.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-git clone https://github.com/PDNMX/piloto_sistema_catalogos.git
-git clone https://github.com/PDNMX/piloto_sistema_oauth20.git
-git clone https://github.com/PDNMX/piloto_sistema_backend.git
-git clone https://github.com/PDNMX/piloto_sistema_frontend.git
+# git clone https://github.com/PDNMX/piloto_sistema_catalogos.git
+# git clone https://github.com/PDNMX/piloto_sistema_oauth20.git
+# git clone https://github.com/PDNMX/piloto_sistema_backend.git
+# git clone https://github.com/PDNMX/piloto_sistema_frontend.git
+
+git clone git@github.com:PDNMX/piloto_sistema_oauth20.git oauth
+git clone git@github.com:PDNMX/piloto_sistema_catalogos.git catalogos
+git clone git@github.com:PDNMX/piloto_sistema_backend.git backend
+git clone git@github.com:PDNMX/piloto_sistema_frontend.git frontend
